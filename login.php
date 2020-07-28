8eb255669e74b5dee91824c7501fc3e73f5da4cb
cesar <a301495@uach.mx>
2020-07-27 20:52:15 -0600

    lalal

---
+++ login.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="login.html" method="post"></form>
+    <input type="text" name="email" placeholder="enter your email">
+    <input type="password" name="password" placeholder="enter your password">
+    <input type="submint" value="Send">
+</body>
+</html>
